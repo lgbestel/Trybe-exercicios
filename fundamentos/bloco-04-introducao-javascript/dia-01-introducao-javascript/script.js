16177dedf5bfc55815e02ef096397410dd4b9cd7
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-04-06 18:33:44 -0300

    exercicios do 1 ao 5

--- fundamentos/bloco-04-introducao-javascript/dia-01-introducao-javascript/script.js
+++ fundamentos/bloco-04-introducao-javascript/dia-01-introducao-javascript/script.js
@@ -1,7 +1,51 @@
-let myName = "Luiz";
-let birthCity = "Curitiba";
-let birthYear = 1992;
+// exercicio 1
+const a = 60;
+const b = 60;
+const c = -2;
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+// exercicio 2
+if (a > b) {
+    console.log(a);
+    }
+    else {
+    console.log(b);
+    }
+
+// exercicio 3    
+if (a > b && a > c) {
+    console.log(a);
+    }
+    else if (b > c) {
+    console.log(b);
+    }
+    else {
+    console.log(c)
+    }
+
+// exercicio 4    
+if (a > 0) {
+    console.log("positive");
+}
+else if (a < 0) {
+    console.log("negative");
+}
+else {
+    console.log("zero");
+}
+
+//exercicio 5
+if ((a*b*c) < 0) {
+    console.log("invalido");
+}
+else if (a + b + c == 180) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
---
+++ fundamentos/bloco-04-introducao-javascript/dia-01-introducao-javascript/script1.js
@@ -0,0 +1,7 @@
+let myName = "Luiz";
+let birthCity = "Curitiba";
+let birthYear = 1992;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
