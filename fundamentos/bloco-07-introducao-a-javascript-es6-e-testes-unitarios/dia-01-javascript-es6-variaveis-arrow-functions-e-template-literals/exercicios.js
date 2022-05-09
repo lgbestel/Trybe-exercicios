4dbbe398f95c0234487b19fc3603554cf111b0f2
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-05-09 10:17:22 -0300

    exercicio 2.2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-variaveis-arrow-functions-e-template-literals/exercicios.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-variaveis-arrow-functions-e-template-literals/exercicios.js
@@ -1,38 +1,62 @@
 
-//1
-// function testingScope(escopo) {
-//   if (escopo === true) {
-//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-//     console.log(ifScope);
-//   } else {
-//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//     console.log(elseScope);
-//   }
-//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-// }
+//1.1
+function testingScope(escopo) {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
 
-// testingScope(true);
+testingScope(true);
 
-// const testingScope = (escopo) => {
-//   if (escopo === true) {
-//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-//     console.log(ifScope);
-//   } else {
-//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//     console.log(elseScope);
-//   }
-// }
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
 
-// testingScope(true);
+testingScope(true);
 
-//2
+//1.2
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const arraySort = (arr) => {
-  const reversedArr = arr.sort(function(a, b){return a-b});
+  const reversedArr = arr.sort((a, b) => a-b);
   return reversedArr;
 }
 
 console.log(`Os números ${arraySort(oddsAndEvens)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
+
+//2.1
+const fatorial = (num) => {
+  let result = 1;
+  for (let i = 2; i <= num; i += 1) {
+    result *= i;
+  }
+  return result;
+}
+console.log(fatorial(4));
+
+//2.2
+const biggestWord = (string) => {
+  const splittedString = string.split(' ');
+  let word = '';
+
+  for (let i = 0; i < splittedString.length; i += 1) {
+    if (splittedString[i].length > word.length) {
+      word = splittedString[i];
+    }
+  }
+  return word;
+}; 
+console.log(biggestWord ('abaa aaca abac casasca a'));
