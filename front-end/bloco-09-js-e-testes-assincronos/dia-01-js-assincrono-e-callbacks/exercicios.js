cf511de9cd6dcac635535733e3f57431e233a937
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-06-01 08:53:52 -0300

    exercicios 9.1

---
+++ front-end/bloco-09-js-e-testes-assincronos/dia-01-js-assincrono-e-callbacks/exercicios.js
@@ -0,0 +1,39 @@
+//3
+// const getPlanet = () => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   setTimeout(() => console.log("Returned planet: ", mars), 4000);
+// };
+
+// getPlanet() // imprime Marte depois de 4 segundos
+
+//4, 5 e 6
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = (callback, err) => {
+  const temp = getMarsTemperature();
+  setTimeout(() => callback(temp), messageDelay())
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+
