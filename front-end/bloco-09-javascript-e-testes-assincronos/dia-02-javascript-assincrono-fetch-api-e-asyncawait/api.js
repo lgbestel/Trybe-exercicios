47bbb2f631d7da97489aea2357369c43a8298eb7
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-06-02 11:44:10 -0300

    exercicios dia 9.2 finalizados

---
+++ front-end/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -0,0 +1,19 @@
+const url = `https://api.coincap.io/v2/assets`;
+const ul = document.querySelector('ul');
+
+const coinLi = (data) => {
+  data.filter(({ rank }) => rank <= 10)
+    .forEach(({ name, symbol, priceUsd }) => {
+      const li = document.createElement('li');
+      li.innerHTML =  `${name} (${symbol}): USD ${priceUsd}`;
+      ul.appendChild(li);
+    })
+}
+
+const arrCoins = () => {
+  const coinsArray = fetch(url)
+    .then((response) => response.json())
+    .then((data) => coinLi(data.data))
+    .catch((error) => console.log('deu ruim :', error));
+};
+arrCoins();
