15e211cf1396896917f55116657d3a2868b1386d
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-06-20 10:08:39 -0300

    iniciando o dia 10.1

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/appteste/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TestandoReact from './header';
+
+function App() {
+  return (
+    <>
+      <TestandoReact />
+      Hello, World!
+    </>
+  );
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/appteste/src/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TestandoReact extends React.Component {
+  render () {
+    return <header>
+      <h1>
+        Olá, Galera!
+      </h1>
+    </header>
+  }
+}
+
+export default TestandoReact
